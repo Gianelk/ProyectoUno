ec08bdfb0227934a4eac3c1113ad0afae905158e
LKIX <115419842+LKIX@users.noreply.github.com>
2024-05-31 22:45:08 -0400

    Añadi clase toma2 y toma4. Tambien funciones en el main

---
+++ ProyectoUno/src/Toma2.java
@@ -0,0 +1,5 @@
+public class Toma2 extends Comodin{
+    public Toma2(String numeroCarta, String color) {
+        super(numeroCarta, color);
+    }
+}
---
+++ ProyectoUno/src/Toma4.java
@@ -0,0 +1,5 @@
+public class Toma4 extends Comodin{
+    public Toma4(String numeroCarta, String color) {
+        super(numeroCarta, color);
+    }
+}
