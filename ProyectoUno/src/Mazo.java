2d6870b45cfc70a96f343788d6620228a1bcdf24
Gianelk <gaelkareh.23@est.ucab.edu.ve>
2024-05-31 22:21:35 -0400

    Clase Mazo

--- ProyectoUno/src/Mazo.java
+++ ProyectoUno/src/Mazo.java
@@ -1,2 +1,39 @@
-public class Mazo {
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Mazo extends LinkedList<Carta>{
+    LinkedList<Carta> mazo;
+
+    public Mazo(LinkedList<Carta> mazo) {
+        this.mazo = mazo;
+    }
+
+    public LinkedList<Carta> getMazo() {
+        return mazo;
+    }
+
+    public void setMazo(LinkedList<Carta> mazo) {
+        this.mazo = mazo;
+    }
+    public void barajar(){
+        Random random = new Random();
+        Carta cartaAuxiliar=new Carta();
+        for(int  i= 0;i<mazo.size(); i++) {
+            int numero = random.nextInt(mazo.size());
+            if (i != numero) {
+                cartaAuxiliar=mazo.get(i);
+                mazo.remove(i);
+                mazo.add(numero, cartaAuxiliar);
+            }
+        }
+    }
+    public  void mostrarMiLista() {
+        System.out.println("Los Elementos de la lista son: ");
+        System.out.println();
+        for(int  i= 0;i<mazo.size(); i++) {
+            System.out.println(mazo.get(i).numeroCarta);
+            System.out.println(mazo.get(i).color);
+        }
+
+    }
 }
