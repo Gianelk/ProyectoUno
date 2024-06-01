9155262d1b0f2aaa308cbb14560a032bb3f96e74
Gianelk <gaelkareh.23@est.ucab.edu.ve>
2024-05-31 22:31:00 -0400

    Procedimiento crear cartas

--- ProyectoUno/src/Main.java
+++ ProyectoUno/src/Main.java
@@ -1,5 +1,28 @@
 
 public class Main {
+    public static void crearCartas(String color,LinkedList<Carta> cartas) {
+    String numeroCarta;
+    for (int i = 0; i < 10; i++) {
+        String pasaTurno = Character.toString((char) 169);
+        numeroCarta = String.valueOf(i);
+        Carta carta = new Carta(numeroCarta, color);
+        if (i == 0) {
+            cartas.add(carta);
+            carta = new Carta("+2", color);
+            cartas.add(carta);
+            cartas.add(carta);
+            carta = new Carta("<>", color);
+            cartas.add(carta);
+            cartas.add(carta);
+            carta = new Carta(pasaTurno, color);
+            cartas.add(carta);
+            cartas.add(carta);
+        } else {
+            cartas.add(carta);
+            cartas.add(carta);
+        }
+    }
+}
     public static void main(String[] args) {
 
     }
