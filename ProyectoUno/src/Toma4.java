f2a313ae3ccdce46da31251bd1ca237ace3c8617
LKIX <115419842+LKIX@users.noreply.github.com>
2024-06-02 19:26:21 -0400

    Toma 4 metodo

--- ProyectoUno/src/Toma4.java
+++ ProyectoUno/src/Toma4.java
@@ -2,4 +2,15 @@ public class Toma4 extends Comodin{
     public Toma4(String numeroCarta, String color) {
         super(numeroCarta, color);
     }
+    public void tomar4Cartas(Jugador jugador, Mazo mazo){
+        Carta carta;
+        Baraja baraja;
+        baraja=jugador.cartasDisponibles;
+        int i=0;
+        for (i=0; i<4; i++){
+            carta=mazo.getPrimeraMazo(0);
+            baraja.add(carta);
+            mazo.eliminarPrimeraCarta();
+        }
+    }
 }
