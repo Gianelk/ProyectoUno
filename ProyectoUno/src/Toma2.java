91757669fb51a9cbb14d17c72945bd751d0bcfdd
FernandL9 <faliendo.23@est.ucab.edu.ve>
2024-06-02 19:23:50 -0400

    Comodines agregación de métodos

--- ProyectoUno/src/Toma2.java
+++ ProyectoUno/src/Toma2.java
@@ -1,5 +1,18 @@
 public class Toma2 extends Comodin{
     public Toma2(String numeroCarta, String color) {
+
         super(numeroCarta, color);
+    }
+
+    public void tomar2Cartas(Jugador jugador, Mazo mazo){
+        Carta carta;
+        Baraja baraja;
+        baraja=jugador.cartasDisponibles;
+        int i;
+        for (i=0; i<2; i++){
+            carta = mazo.getPrimeraMazo(0);
+            baraja.add(carta);
+            mazo.eliminarPrimeraCarta();
+        }
     }
 }
