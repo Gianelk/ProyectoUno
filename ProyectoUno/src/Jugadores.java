95608b9057b9ff13dea35f12d79990c223119383
FernandL9 <faliendo.23@est.ucab.edu.ve>
2024-06-03 18:14:00 -0400

    Arreglar conflicto

--- ProyectoUno/src/Jugadores.java
+++ ProyectoUno/src/Jugadores.java
@@ -8,7 +8,6 @@ public class Jugadores extends LinkedList<Jugador>{
     public LinkedList<Jugador> getJugadores() {
         return jugadores;
     }
-
     public void setJugadores(LinkedList<Jugador> jugadores) {
         this.jugadores = jugadores;
     }
@@ -18,13 +17,21 @@ public class Jugadores extends LinkedList<Jugador>{
     public Jugadores(LinkedList<Jugador> jugadores) {
         this.jugadores = jugadores;
     }
+<<<<<<< HEAD
+=======
+
+    public Jugador getJugadores(int j){
+        return jugadores.get(j);
+    }
+>>>>>>> 21a6007 (Arreglo jugadores V2)
     public void crearJugador(){
         Scanner leer = new Scanner(System.in);
         System.out.println("Ingrese el nombre del jugador");
-        String nombreJugador = leer.toString();
+        String nombreJugador = leer.next();
         Jugador jugador = new Jugador(nombreJugador);
         jugadores.add(jugador);
         jugador = new Jugador("Joselito bot");
+        jugadores.add(jugador);
     }
     public  void mostrarMiLista() {
         System.out.println("Los jugadores de la lista son: ");
@@ -32,6 +39,6 @@ public class Jugadores extends LinkedList<Jugador>{
         for(int  i= 0;i<jugadores.size(); i++) {
             System.out.println(jugadores.get(i).nombre);
         }
-
     }
+
 }
